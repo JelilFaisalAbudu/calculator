760c24f360c7726b90a2ea76f6e5a194cd3e7add
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-01-08 08:59:15 +0000

    include hot reload module for the page

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'),
+);
+
+if (module.hot) {
+  module.hot.accept();
+}
