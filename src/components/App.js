f60867f09419f2819f9b4327a729c2cdbf09f8bd
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-01-12 19:57:17 +0000

    Change App component to stateful component

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,41 @@ import ButtonPanel from './ButtonPanel';
 /* eslint-disable no-unused-vars */
 import calculate from '../logic/calculate';
 
-function App() {
-  return (
-    <div>
-      <Display result="23" />
-      <ButtonPanel />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operation } = this.state;
+
+    this.setState(calculate({ total, next, operation }, buttonName));
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    let result = total || next;
+
+    if (total && next && operation) {
+      result = `${total} ${operation} ${next}`;
+    } else if (total && operation) {
+      result = `${total} ${operation}`;
+    }
+    return (
+      <div className="App">
+        <Display result={result || undefined} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
