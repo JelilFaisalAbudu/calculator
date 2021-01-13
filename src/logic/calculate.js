30985158f5d3acb0c27a0f236f5c7989bac42c4a
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-01-13 10:07:30 +0000

    Add operations to the calculate module

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,16 +1,59 @@
 import operate from './operate';
 
 const calculate = (dataObj, buttonName) => {
-  const { total, next, operation } = dataObj;
-
-  if (buttonName === '+/-') {
-    if (total) {
-      return total * -1;
+  let { total, next, operation } = dataObj;
+  const operands = ['+', '-', '÷', 'x', '%'];
+  if (buttonName === 'AC' || total === '∞') {
+    total = null;
+    next = null;
+    operation = null;
+  } else if (buttonName === '=') {
+    if (total && next && operation) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
     }
-    return next * -1;
+  } else if (operands.includes(buttonName)) {
+    if (buttonName === '%') {
+      if (!next) {
+        next = total;
+      }
+      operation = buttonName;
+      total = operate(null, next, operation);
+      next = null;
+      operation = null;
+    } else if (total && next && operation) {
+      total = operate(total, next, operation);
+      operation = buttonName;
+      next = null;
+    } else if (next && !operation) {
+      total = next;
+      operation = buttonName;
+      next = null;
+    } else {
+      operation = buttonName;
+    }
+  } else if (buttonName === '+/-') {
+    if (next) {
+      next = operate(next, '-1', 'x');
+    } else if (total) {
+      total = operate(total, '-1', 'x');
+    }
+  } else if (next === null && operation === null && total) {
+    next = buttonName;
+    total = null;
+  } else if (next) {
+    if (buttonName !== '.' || (buttonName === '.' && next.indexOf(buttonName) < 0)) {
+      next = next.concat(buttonName);
+    }
+  } else {
+    next = buttonName;
   }
-
-  return operate(total, next, operation);
+  return {
+    total,
+    next,
+    operation,
+  };
 };
 
 export default calculate;
